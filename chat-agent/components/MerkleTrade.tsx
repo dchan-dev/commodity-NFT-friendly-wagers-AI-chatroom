ea96fb8fac205a3db316ea44a0a61a4e2bbefec9
danny <danny.chan@codpayment.com>
2025-02-20 07:43:54 +0800

    add merkletrade/ts-sdk

--- chat-agent/components/MerkleTrade.tsx
+++ chat-agent/components/MerkleTrade.tsx
@@ -8,7 +8,9 @@ const aptos = new Aptos(merkle.config.aptosConfig);
 
 // initialize account (refer to Aptos docs)
 
-const account = ...
+const account = {
+  accountAddress: "@0x000000000000000000000000000000000000000000000000000000000000000A"
+};
 
 // Place a market order
 
@@ -23,10 +25,12 @@ const order = await merkle.payloads.placeMarketOrder({
 
 // submit transaction
 
+const accountSigner = aptos.deriveAccountSigner(account);
+
 const committedTransaction = await aptos.transaction.build
-  .simple({ sender: account.accountAddress, data: payload })
+  .simple({ sender: account.accountAddress, data: order })
   .then((transaction) =>
-    aptos.signAndSubmitTransaction({ signer: account, transaction }),
+    aptos.signAndSubmitTransaction({ signer: accountSigner, transaction }),
   )
   .then(({ hash }) => aptos.waitForTransaction({ transactionHash: hash }));
 
